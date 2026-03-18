a4c212ae5f1c4a0e1bab951df975b69f86f426b9
Pankaj Ushir <ushirpankaj98@gmail.com>
2026-03-18 11:21:20 +0530

    comment for id auto-increament

--- backend/models/Task.py
+++ backend/models/Task.py
@@ -3,7 +3,7 @@ from app import db
 class Task(db.Model):
     __tablename__ = "tasks"
     
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) #Automatically added autoincreament
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
     is_completed = db.Column(db.Boolean, default=False)
