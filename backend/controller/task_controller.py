9913f91e455132814d1c719378e5ca9cef537562
Pankaj Ushir <ushirpankaj98@gmail.com>
2026-03-18 11:24:45 +0530

    add the update API and fix the getall and getone API

--- backend/controller/task_controller.py
+++ backend/controller/task_controller.py
@@ -8,7 +8,17 @@ def home():
 
 @app.route("/tasks",methods=['GET'])
 def getAllTasks():
-    return Task.query.all()
+    tasks =  Task.query.all()
+    result = []
+    for t in tasks:
+        result.append({
+            "id": t.id,
+            "title": t.title,
+            "description": t.description,
+            "is_completed": t.is_completed,
+            "created_at": t.created_at
+        })
+    return result
 
 @app.route("/task/<int:id>", methods=['GET'])
 def getTask(id):
@@ -20,7 +30,8 @@ def getTask(id):
     return jsonify({
         "id": task.id,
         "title": task.title,
-        "completed": task.completed
+        "description":task.description,
+        "is_completed": task.is_completed
     })
 
 @app.route("/task/create", methods=["POST"])
@@ -34,8 +45,18 @@ def createTask():
 
     return {"msg": "Task Created", "id": task.id}, 201
 
+@app.route("/task/update/<int:id>",methods=["PUT"])
+def updateTask(id):
+    data = request.get_json()
+    task = Task.query.get(id)
+    if not task:
+        return jsonify({"error": "Task not found"}), 404
+    task.title = data.get('title',task.title)
+    task.description = data.get('description', task.description)
+    task.is_completed = data.get('is_completed', task.is_completed)
+    db.session.commit()
 
-
+    return {"msg": "Task updated"}
 
 @app.route('/task/<int:id>', methods=['DELETE'])
 def delete_task(id):
