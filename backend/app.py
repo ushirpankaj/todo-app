8f8e170ad0859c8968ed2476cb23f65f01812943
Pankaj Ushir <ushirpankaj98@gmail.com>
2026-03-26 16:38:18 +0530

    jwt added in app

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
@@ -13,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db) 
+jwt = JWTManager(app)
 
 from models import *
 from controller import *
