08bd982d3c3e16042f87d06112d68f3cd2205730
Pankaj Ushir <ushirpankaj98@gmail.com>
2026-03-26 16:38:48 +0530

    Login API added

--- backend/controller/auth_controller.py
+++ backend/controller/auth_controller.py
@@ -1,7 +1,8 @@
 from app import app, db
 from flask import jsonify, request
 from models.User import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
 
 @app.route("/register", methods=['POST'])
 def register():
@@ -14,4 +15,27 @@ def register():
     db.session.commit()
     return {"message": "User Created", "id": user.id}, 201
 
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data["username"]).first()
 
+    
+    if not user and not check_password_hash(user.password, data["password"]):
+        return jsonify({"error":"Invalid credentials"}),401
+    
+    access_token = create_access_token(
+        identity=user.id,
+        additional_claims={
+        "username": user.username,
+        "email": user.email,
+        "first_name" : user.first_name,
+        "last_name" : user.last_name,
+        "phone_number" : user.phone_number
+        }
+    )
+    return {
+        "jwt_access_token":access_token,
+        "message" : "Login Successful"
+    }
+        
